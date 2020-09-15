410c83f112da51e3903a82e2a69ba64c441296ee
Kenneth Skovhede <kenneth@hexad.dk>
2020-09-15 09:52:31 +0200

    Fixed listening to Unix Domain Socket, and added support for auto-deleting an unused domain socket

--- src/AsyncListenSocket.cs
+++ src/AsyncListenSocket.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Net.Sockets;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
@@ -16,23 +17,37 @@ namespace SockRock
         /// <summary>
         /// The socket we are bound to
         /// </summary>
-        private readonly int m_socket;
+        private int m_socket = -1;
 
         /// <summary>
         /// A monitor used to get async triggers from the monitor
         /// </summary>
-        private readonly MonitoredHandle m_handle;
+        private MonitoredHandle m_handle;
+
+        /// <summary>
+        /// The handler to use
+        /// </summary>
+        private readonly ISocketHandler m_handler;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="T:SockRock.ListenSocket"/> class.
         /// </summary>
         public AsyncListenSocket(ISocketHandler handler)
         {
-            if (handler == null)
-                throw new ArgumentNullException(nameof(handler));
+            m_handler = handler ?? throw new ArgumentNullException(nameof(handler));
+        }
+
+        /// <summary>
+        /// Helper method to set up the socket when calling bind
+        /// </summary>
+        /// <param name="family">The address family to use</param>
+        private void SetupSocket(UnixAddressFamily family)
+        {
+            if (m_socket != -1)
+                throw new InvalidOperationException("The socket is already initialized");
 
             // Create new socket
-            m_socket = Syscall.socket(UnixAddressFamily.AF_INET, UnixSocketType.SOCK_STREAM, 0);
+            m_socket = Syscall.socket(family, UnixSocketType.SOCK_STREAM, 0);
 
             // Allow address reuse
             Syscall.setsockopt(m_socket, UnixSocketProtocol.SOL_SOCKET, UnixSocketOptionName.SO_REUSEADDR, 1);
@@ -46,52 +61,117 @@ namespace SockRock
             if (Syscall.fcntl(m_socket, FcntlCommand.F_SETFL, opts) < 0)
                 throw new IOException($"Failed to set socket O_NOBLOCK: {Stdlib.GetLastError()}");
 
-            m_handle = handler.MonitoredHandle(m_socket);
+            m_handle = m_handler.MonitoredHandle(m_socket);
         }
 
         /// <summary>
         /// Creates a socket listening to the supplied endpoint
         /// </summary>
-        /// <returns>The and accept async.</returns>
         /// <param name="endpoint">The endpoint we listen to.</param>
         /// <param name="backlog">The connection backlog</param>
         public void Bind(EndPoint endpoint, int backlog)
+            => Bind(endpoint, backlog, true);
+
+        /// <summary>
+        /// Creates a socket listening to the supplied endpoint
+        /// </summary>
+        /// <param name="endpoint">The endpoint we listen to.</param>
+        /// <param name="backlog">The connection backlog</param>
+        /// <param name="autodeletedomainsocket">A flag indicating if the domain socket should be attempted deleted</param>
+        public void Bind(EndPoint endpoint, int backlog, bool autodeletedomainsocket)
         {
-            Sockaddr servaddr;
-
-            if (endpoint is IPEndPoint ipe)
+            try
             {
-                // Set up the IP address we are listening on
-                servaddr = new SockaddrIn()
+                Sockaddr servaddr;
+                var filepath = string.Empty;
+
+                if (endpoint is IPEndPoint ipe)
                 {
-                    sa_family = UnixAddressFamily.AF_INET,
-                    sin_family = UnixAddressFamily.AF_INET,
-                    sin_addr = new InAddr() { s_addr = BitConverter.ToUInt32(ipe.Address.GetAddressBytes(), 0) },
-                    sin_port = Syscall.htons((ushort)ipe.Port)
-                };
-            }
-            else if (endpoint is UnixEndPoint upe)
-            {
-                var isHidden = upe.Filename[0] == 0;
-                servaddr = new SockaddrUn(upe.Filename.Substring(isHidden ? 1 : 0), isHidden);
-            }
-            else if (endpoint is System.Net.Sockets.UnixDomainSocketEndPoint udse)
-            {
-                var name = udse.ToString();
-                var isHidden = name[0] == 0 || name[0] == '@';
-                servaddr = new SockaddrUn(name.Substring(isHidden ? 1 : 0), isHidden);
-            }
-            else
-                throw new NotSupportedException($"EndPoint not supported: {endpoint}");
+                    // Set up the IP address we are listening on
+                    servaddr = new SockaddrIn()
+                    {
+                        sa_family = UnixAddressFamily.AF_INET,
+                        sin_family = UnixAddressFamily.AF_INET,
+                        sin_addr = new InAddr() { s_addr = BitConverter.ToUInt32(ipe.Address.GetAddressBytes(), 0) },
+                        sin_port = Syscall.htons((ushort)ipe.Port)
+                    };
 
-            // Bind so we are attached
-            var ret = Syscall.bind(m_socket, servaddr);
-            if (ret < 0)
-                throw new IOException($"Failed to bind to endpoint: {Stdlib.GetLastError()}");
+                    SetupSocket(UnixAddressFamily.AF_INET);
+                }
+                else if (endpoint is UnixEndPoint upe)
+                {
+                    var isHidden = upe.Filename[0] == 0;
+                    if (!isHidden)
+                        filepath = upe.Filename;
+                    servaddr = new SockaddrUn(upe.Filename.Substring(isHidden ? 1 : 0), isHidden);
+                    SetupSocket(UnixAddressFamily.AF_UNIX);
+                }
+                else if (endpoint is System.Net.Sockets.UnixDomainSocketEndPoint udse)
+                {
+                    var name = udse.ToString();
+                    var isHidden = name[0] == 0 || name[0] == '@';
+                    if (!isHidden)
+                        filepath = name;
+                    servaddr = new SockaddrUn(name.Substring(isHidden ? 1 : 0), isHidden);
+                    SetupSocket(UnixAddressFamily.AF_UNIX);
+                }
+                else
+                    throw new NotSupportedException($"EndPoint not supported: {endpoint.GetType()}");
 
-            ret = Syscall.listen(m_socket, backlog);
-            if (ret < 0)
-                throw new IOException($"Failed to set socket to listen: {Stdlib.GetLastError()}");
+                // Bind so we are attached
+                var ret = Syscall.bind(m_socket, servaddr);
+                if (ret < 0)
+                {
+                    // Save original error code
+                    var err = Stdlib.GetLastError();
+
+                    // Handle auto-delete of sockets
+                    if (autodeletedomainsocket && err == Errno.EADDRINUSE && !string.IsNullOrWhiteSpace(filepath) && File.Exists(filepath) && new FileInfo(filepath).Length == 0)
+                    {
+                        // Ensure that no other process is using the socket
+                        var csock = Syscall.socket(UnixAddressFamily.AF_UNIX, UnixSocketType.SOCK_STREAM, 0);
+                        ret = Syscall.connect(csock, servaddr);
+                        Syscall.close(csock);
+
+                        // If we cannot connect, assume it is broken
+                        if (ret != 0)
+                        {
+                            // Try to remove the file
+                            var failed = false;
+                            try { File.Delete(filepath); }
+                            catch { failed = true; }
+
+                            // If the file is removed, try binding again
+                            if (!failed)
+                            {
+                                Syscall.close(m_socket);
+                                m_socket = -1;
+                                Bind(endpoint, backlog, false);
+                                return;
+                            }
+                        }
+                    }
+
+                    throw new IOException($"Failed to bind to endpoint: {err}");
+                }
+
+                ret = Syscall.listen(m_socket, backlog);
+                if (ret < 0)
+                    throw new IOException($"Failed to set socket to listen: {Stdlib.GetLastError()}");
+            }
+            catch
+            {
+                // Avoid leaving the listensocket in an invalid state
+                if (m_socket != -1)
+                {
+                    try { Syscall.close(m_socket); }
+                    catch {}
+
+                    m_socket = -1;
+                }
+                throw;
+            }
+
         }
 
         /// <summary>
@@ -103,7 +183,17 @@ namespace SockRock
         /// so the garbage collector can reclaim the memory that the <see cref="T:SockRock.ListenSocket"/> was occupying.</remarks>
         public void Dispose()
         {
-            Syscall.close(m_socket);
+            if (m_socket != -1)
+            {
+                try
+                {
+                    Syscall.close(m_socket);
+                }
+                finally
+                {
+                    m_socket = -1;
+                }
+            }
         }
 
         /// <summary>
--- src/ListenSocket.cs
+++ src/ListenSocket.cs
@@ -16,16 +16,26 @@ namespace SockRock
         /// <summary>
         /// The socket we are bound to
         /// </summary>
-        private readonly int m_socket;
+        private int m_socket = -1;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="T:SockRock.ListenSocket"/> class.
         /// </summary>
         public ListenSocket()
         {
-            // Create new socket
-            m_socket = Syscall.socket(UnixAddressFamily.AF_INET, UnixSocketType.SOCK_STREAM, 0);
+        }
 
+        /// <summary>
+        /// Helper method to set up the socket when calling bind
+        /// </summary>
+        /// <param name="family">The address family to use</param>
+        private void SetupSocket(UnixAddressFamily family)
+        {
+            if (m_socket != -1)
+                throw new InvalidOperationException("The socket is already initialized");
+
+            // Create the socket
+            m_socket = Syscall.socket(family, UnixSocketType.SOCK_STREAM, 0);
             // Allow address reuse
             Syscall.setsockopt(m_socket, UnixSocketProtocol.SOL_SOCKET, UnixSocketOptionName.SO_REUSEADDR, 1);
         }
@@ -33,46 +43,113 @@ namespace SockRock
         /// <summary>
         /// Creates a socket listening to the supplied endpoint
         /// </summary>
-        /// <returns>The and accept async.</returns>
         /// <param name="endpoint">The endpoint we listen to.</param>
         /// <param name="backlog">The connection backlog</param>
         public void Bind(EndPoint endpoint, int backlog)
+            => Bind(endpoint, backlog, true);
+
+        /// <summary>
+        /// Creates a socket listening to the supplied endpoint
+        /// </summary>
+        /// <param name="endpoint">The endpoint we listen to.</param>
+        /// <param name="backlog">The connection backlog</param>
+        /// <param name="autodeletedomainsocket">A flag indicating if the domain socket should be attempted deleted</param>
+        public void Bind(EndPoint endpoint, int backlog, bool autodeletedomainsocket)
         {
-            Sockaddr servaddr;
-
-            if (endpoint is IPEndPoint ipe)
+            try
             {
-                // Set up the IP address we are listening on
-                servaddr = new SockaddrIn()
+                Sockaddr servaddr;
+                var filepath = string.Empty;
+
+                if (endpoint is IPEndPoint ipe)
                 {
-                    sa_family = UnixAddressFamily.AF_INET,
-                    sin_family = UnixAddressFamily.AF_INET,
-                    sin_addr = new InAddr() { s_addr = BitConverter.ToUInt32(ipe.Address.GetAddressBytes(), 0) },
-                    sin_port = Syscall.htons((ushort)ipe.Port)
-                };
-            }
-            else if (endpoint is UnixEndPoint upe)
-            {
-                var isHidden = upe.Filename[0] == 0;
-                servaddr = new SockaddrUn(upe.Filename.Substring(isHidden ? 1 : 0), isHidden);
-            }
-            else if (endpoint is System.Net.Sockets.UnixDomainSocketEndPoint udse)
-            {
-                var name = udse.ToString();
-                var isHidden = name[0] == 0 || name[0] == '@';
-                servaddr = new SockaddrUn(name.Substring(isHidden ? 1 : 0), isHidden);
-            }
-            else
-                throw new NotSupportedException($"EndPoint not supported: {endpoint}");
+                    // Set up the IP address we are listening on
+                    servaddr = new SockaddrIn()
+                    {
+                        sa_family = UnixAddressFamily.AF_INET,
+                        sin_family = UnixAddressFamily.AF_INET,
+                        sin_addr = new InAddr() { s_addr = BitConverter.ToUInt32(ipe.Address.GetAddressBytes(), 0) },
+                        sin_port = Syscall.htons((ushort)ipe.Port)
+                    };
 
-            // Bind so we are attached
-            var ret = Syscall.bind(m_socket, servaddr);
-            if (ret < 0)
-                throw new IOException($"Failed to bind to endpoint: {Stdlib.GetLastError()}");
+                    // Prepare the socket for TCP/IP
+                    SetupSocket(UnixAddressFamily.AF_INET);
+                }
+                else if (endpoint is UnixEndPoint upe)
+                {
+                    var isHidden = upe.Filename[0] == 0;
+                    if (!isHidden)
+                        filepath = upe.Filename;
+                    servaddr = new SockaddrUn(upe.Filename.Substring(isHidden ? 1 : 0), isHidden);
+                    // Prepare the socket for Unix Domain Socket
+                    SetupSocket(UnixAddressFamily.AF_UNIX);
+                }
+                else if (endpoint is System.Net.Sockets.UnixDomainSocketEndPoint udse)
+                {
+                    var name = udse.ToString();
+                    var isHidden = name[0] == 0 || name[0] == '@';
+                    if (!isHidden)
+                        filepath = name;
+                    servaddr = new SockaddrUn(name.Substring(isHidden ? 1 : 0), isHidden);
+                    // Prepare the socket for Unix Domain Socket
+                    SetupSocket(UnixAddressFamily.AF_UNIX);
+                }
+                else
+                    throw new NotSupportedException($"EndPoint not supported: {endpoint.GetType()}");
 
-            ret = Syscall.listen(m_socket, backlog);
-            if (ret < 0)
-                throw new IOException($"Failed to set socket to listen: {Stdlib.GetLastError()}");
+                // Bind so we are attached
+                var ret = Syscall.bind(m_socket, servaddr);
+                if (ret < 0)
+                {
+                    // Save original error code
+                    var err = Stdlib.GetLastError();
+
+                    // Handle auto-delete of sockets
+                    if (autodeletedomainsocket && err == Errno.EADDRINUSE && !string.IsNullOrWhiteSpace(filepath) && File.Exists(filepath) && new FileInfo(filepath).Length == 0)
+                    {
+                        // Ensure that no other process is using the socket
+                        var csock = Syscall.socket(UnixAddressFamily.AF_UNIX, UnixSocketType.SOCK_STREAM, 0);
+                        ret = Syscall.connect(csock, servaddr);
+                        Syscall.close(csock);
+
+                        // If we cannot connect, assume it is broken
+                        if (ret != 0)
+                        {
+                            // Try to remove the file
+                            var failed = false;
+                            try { File.Delete(filepath); }
+                            catch { failed = true; }
+
+                            // If the file is removed, try binding again
+                            if (!failed)
+                            {
+                                Syscall.close(m_socket);
+                                m_socket = -1;
+                                Bind(endpoint, backlog, false);
+                                return;
+                            }
+                        }
+                    }
+
+                    throw new IOException($"Failed to bind to endpoint: {err}");
+                }
+
+                ret = Syscall.listen(m_socket, backlog);
+                if (ret < 0)
+                    throw new IOException($"Failed to set socket to listen: {Stdlib.GetLastError()}");
+            }
+            catch
+            {
+                // Avoid leaving the listensocket in an invalid state
+                if (m_socket != -1)
+                {
+                    try { Syscall.close(m_socket); }
+                    catch {}
+
+                    m_socket = -1;
+                }
+                throw;
+            }
         }
 
         /// <summary>
@@ -84,7 +161,17 @@ namespace SockRock
         /// so the garbage collector can reclaim the memory that the <see cref="T:SockRock.ListenSocket"/> was occupying.</remarks>
         public void Dispose()
         {
-            Syscall.close(m_socket);
+            if (m_socket != -1) 
+            {
+                try 
+                {
+                    Syscall.close(m_socket);
+                }
+                finally
+                {
+                    m_socket = -1;
+                }
+            }
         }
 
         /// <summary>
